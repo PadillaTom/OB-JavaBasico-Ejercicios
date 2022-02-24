7416e1f97f5420c3c6a8b234bd78b0a68d0c68b6
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-24 10:20:15 +0100

    Ejercicio1

---
+++ src/com/padillatomas/ejercicio1/MyApp.java
@@ -0,0 +1,38 @@
+package com.padillatomas.ejercicio1;
+
+public class MyApp {
+    public static void main(String[] args) {
+//        Enteros:
+        byte myByte = 1;
+        short myShort = 2;
+        int myInt = 3;
+        long myLong = 4;
+
+//        Floats:
+        float myFloat = 4.5f;
+        double myDouble = 5.0;
+
+//        Caracteres:
+        char myChar = 'A';
+
+//        Booleans:
+        boolean myTrue = true;
+        boolean myFalse = false;
+
+//        Cadenas:
+        String myString = "Hola Mundo";
+
+
+//        Prints:
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Char: " + myChar);
+        System.out.println("Boolean: " + myTrue);
+        System.out.println("Boolean2: " + myFalse);
+        System.out.println("String: " + myString);
+    }
+}
