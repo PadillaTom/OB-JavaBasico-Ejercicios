08896d5371e1a041663cb34f3174a79ad3b7efef
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-24 11:18:37 +0100

    Ejercicio 5

--- src/com/padillatomas/MyApp.java
+++ src/com/padillatomas/MyApp.java
@@ -1,29 +1,17 @@
 package com.padillatomas;
 
-import com.padillatomas.model.SmartPhone;
-import com.padillatomas.model.SmartWatch;
+import com.padillatomas.interfaces.CocheCRUD;
+import com.padillatomas.interfaces.impl.CocheCRUDImpl;
 
 public class MyApp {
     public static void main(String[] args) {
-        System.out.println("Ejercicio 4:");
+        System.out.println("Ejercicio 5:");
 
-        SmartPhone myPhone = new SmartPhone("Apple", "12 Pro", "128GB", "64GB", "4k");
-        SmartWatch myWatch = new SmartWatch("Apple", "Apple Watch", "64GB", "8GB", true);
+        CocheCRUD myCoche = new CocheCRUDImpl();
 
-        System.out.println("=== Mi Telefono ===");
-        System.out.println("Marca -> " + myPhone.marca);
-        System.out.println("Modelo -> " + myPhone.modelo);
-        System.out.println("Memoria -> " + myPhone.memoria);
-        System.out.println("RAM -> " + myPhone.ram);
-        System.out.println("Calidad Videos -> " + myPhone.calidadVideos);
+        myCoche.save();
+        myCoche.findAll();
+        myCoche.delete();
 
-        System.out.println("");
-
-        System.out.println("=== Mi Reloj ===");
-        System.out.println("Marca" + myWatch.marca);
-        System.out.println("Modelo" + myWatch.modelo);
-        System.out.println("Memoria -> " + myWatch.memoria);
-        System.out.println("RAM -> " + myWatch.ram);
-        System.out.println("Tiene Pulsometro -> " + myWatch.hasPulsometro);
     }
 }
---
+++ src/com/padillatomas/interfaces/CocheCRUD.java
@@ -0,0 +1,7 @@
+package com.padillatomas.interfaces;
+
+public interface CocheCRUD {
+    void save();
+    void findAll();
+    void delete();
+}
